b9834f38d93533c53e1cc03062081a3e142d295b
Shiva <shiva@ifpi.org>
2015-03-18 11:46:21 +0000

    Initial commit

---
+++ src/main/java/org/ifpi/iaps/service/HelloWorld.java
@@ -0,0 +1,12 @@
+package org.ifpi.iaps.service;
+
+/**
+ * Created by Shiva on 20/02/2015.
+ */
+  public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+
+}
