fef30123cb555de46d409ffb86259cbf9c236e40
Shiva <shiva@ifpi.org>
2015-03-18 12:02:31 +0000

    Commit the changes

--- src/main/java/org/ifpi/iaps/service/HelloWorld.java
+++ src/main/java/org/ifpi/iaps/service/HelloWorld.java
@@ -6,7 +6,7 @@ package org.ifpi.iaps.service;
   public class HelloWorld {
 
     public static void main(String[] args){
-        System.out.println("Hello World Git1");
+        System.out.println("Hello World Git1 Shiva");
     }
 
 }
